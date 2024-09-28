52f8b4e46e1e5189262ad979a7e2a60f8de248e9
MATTHΞW <mattxlee@gmail.com>
2024-09-28 20:16:50 +0800

    Add basic parser for args.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
+use clap::{command, Parser};
+
+#[derive(Debug, Parser)]
+#[command(version, about, long_about = None)]
+struct Args {
+    /// address (ip:port) for depc node
+    #[arg(long)]
+    depc_node: String,
+
+    #[arg(long)]
+    local_db: String,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let _ = Args::parse();
 }
