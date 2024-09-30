bc5b878467f3dd8665e6a3ed9f540a5cb547def7
MATTHΞW <mattxlee@gmail.com>
2024-09-30 04:05:41 +0000

    Complete the args for RPC connection.

--- src/main.rs
+++ src/main.rs
@@ -5,10 +5,21 @@ use clap::{command, Parser};
 #[derive(Debug, Parser)]
 #[command(version, about, long_about = None)]
 struct Args {
-    /// address (ip:port) for depc node
+    /// The endpoint (http://ip:port) for depc node
     #[arg(long)]
-    depc_node: String,
-
+    rpc_endpoint: String,
+    /// Use cookie for RPC authentication
+    #[arg(long)]
+    rpc_use_cookie: bool,
+    /// The path string to file `.cookie`
+    #[arg(long)]
+    rpc_cookie_path: String,
+    /// The username for RPC authentication
+    #[arg(long)]
+    rpc_user: String,
+    /// The password for RPC authentication
+    rpc_passwd: String,
+    /// The path string to local database
     #[arg(long)]
     local_db: String,
 }
