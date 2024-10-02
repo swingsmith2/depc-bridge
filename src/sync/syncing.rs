8b98f3387b861703aa87300a04fa8ec97e20e53f
MATTHΞW <mattxlee@gmail.com>
2024-10-02 18:02:32 +0000

    Do not sync the tx from genesis block.

--- src/sync/syncing.rs
+++ src/sync/syncing.rs
@@ -40,41 +40,45 @@ pub async fn sync(
         let block_hash = client.get_block_hash(sync_height)?;
         let block = client.get_block(&block_hash)?;
         assert_eq!(block.height, sync_height);
+
         conn.add_block(&block.hash, sync_height, &block.miner, block.time)?;
 
-        // transactions
-        for txid in block.tx.iter() {
-            let transaction = client.get_transaction(txid)?;
-            assert_eq!(transaction.txid, *txid);
-            conn.add_transaction(&block_hash, txid)?;
-            for txin in transaction.vin.iter() {
-                if !txin.is_coinbase() {
-                    // TODO maybe we need to check the validation of the txin?
-                    conn.mark_coin_to_spent(
-                        &txin.txid.clone().unwrap(),
-                        txin.vout.unwrap(),
-                        txid,
-                        sync_height,
-                    )?;
+        if sync_height > 0 {
+            // transactions
+            for txid in block.tx.iter() {
+                let transaction = client.get_transaction(txid)?;
+                assert_eq!(transaction.txid, *txid);
+                conn.add_transaction(&block_hash, txid)?;
+                for txin in transaction.vin.iter() {
+                    if !txin.is_coinbase() {
+                        // TODO maybe we need to check the validation of the txin?
+                        conn.mark_coin_to_spent(
+                            &txin.txid.clone().unwrap(),
+                            txin.vout.unwrap(),
+                            txid,
+                            sync_height,
+                        )?;
+                    }
                 }
-            }
-            for txout in transaction.vout.iter() {
-                // save the txout anyway
-                if let Some(address) = txout.get_address() {
-                    conn.add_coin(
-                        txid,
-                        txout.n,
-                        txout.value64,
-                        &address,
-                        &txout.script_pubkey.hex,
-                    )?;
-                    // check the coin is mine?
-                    if address == owner_address {
-                        // TODO now check the extra fields to get the ERC20 info.
+                for txout in transaction.vout.iter() {
+                    // save the txout anyway
+                    if let Some(address) = txout.get_address() {
+                        conn.add_coin(
+                            txid,
+                            txout.n,
+                            txout.value64,
+                            &address,
+                            &txout.script_pubkey.hex,
+                        )?;
+                        // check the coin is mine?
+                        if address == owner_address {
+                            // TODO now check the extra fields to get the ERC20 info.
+                        }
                     }
                 }
             }
         }
+
         sync_height += 1;
     }
     conn.commit_transaction()?;
