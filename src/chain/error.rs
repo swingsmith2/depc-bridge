2e0533ed9d60565f4dc7650da4dbcb92b9dcea78
MATTHΞW <mattxlee@gmail.com>
2024-10-02 04:11:38 +0000

    impl Display for Error.

--- src/chain/error.rs
+++ src/chain/error.rs
@@ -2,3 +2,15 @@
 pub enum Error {
     General,
 }
+
+impl std::fmt::Display for Error {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            Error::General => {
+                write!(f, "General error")
+            }
+        }
+    }
+}
+
+impl std::error::Error for Error {}
