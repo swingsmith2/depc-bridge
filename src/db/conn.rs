7c6cca5809415c5ced90d126d4d19e0af2cb0ec3
MATTHΞW <mattxlee@gmail.com>
2024-10-08 04:19:39 +0000

    Add comments.

--- src/db/conn.rs
+++ src/db/conn.rs
@@ -8,18 +8,21 @@ const SQL_ROLLBACK_TRANSACTION: &str = "rollback transaction";
 
 const SQL_COMMIT_TRANSACTION: &str = "commit transaction";
 
+/// Table `blocks`
 const SQL_CREATE_TABLE_BLOCKS: &str =
     "create table if not exists blocks (hash, height, miner, time)";
 const SQL_CREATE_UNIQUE_INDEX_BLOCKS_HASH: &str =
     "create unique index if not exists index__blocks_hash on blocks (hash)";
 const SQL_INSERT_BLOCK: &str = "insert into blocks (hash, height, miner, time) values (?, ?, ?, ?)";
 
+/// Table `transactions`
 const SQL_CREATE_TABLE_TRANSACTIONS: &str =
     "create table if not exists transactions (block_hash, txid)";
 const SQL_CREATE_UNIQUE_INDEX_TRANSACTIONS_TXID: &str =
     "create unique index if not exists index__transactions_txid on transactions (txid)";
 const SQL_INSERT_TRANSACTION: &str = "insert into transactions (block_hash, txid) values (?, ?)";
 
+/// Table `coins`
 const SQL_CREATE_TABLE_COINS: &str =
     "create table if not exists coins (txid, n, value, owner, script_hex, is_spent, spent_height, spent_txid)";
 const SQL_CREATE_UNIQUE_INDEX_COINS_TXID_N: &str =
@@ -35,6 +38,7 @@ const SQL_INSERT_COIN: &str =
 const SQL_MARK_COIN_SPENT: &str =
     "update coins set is_spent = true, spent_txid = ?, spent_height = ? where txid = ? and n = ?";
 
+/// Table `deposit`
 const SQL_CREATE_TABLE_DEPC_DEPOSIT: &str = "create table if not exists depc_deposit (depc_txid, depc_timestamp, from_address_depc, to_address_erc20, amount, erc20_txid, erc20_timestamp)";
 const SQL_CREATE_UNIQUE_INDEX_DEPC_DEPOSIT_DEPC_TXID: &str =
     "create unique index if not exists index__depc_deposit_depc_txid on depc_deposit (depc_txid)";
@@ -42,24 +46,23 @@ const SQL_INSERT_DEPC_DEPOSIT: &str = "insert into depc_deposit (depc_txid, from
 const SQL_UPDATE_DEPC_DEPSOIT: &str =
     "update depc_deposit set erc20_txid = ?, erc20_timestamp = ? where depc_txid = ?";
 
+/// Table `withdraw`
 const SQL_CREATE_TABLE_DEPC_WITHDRAW: &str = "create table if not exists depc_withdraw (erc20_txid, erc20_timestamp, from_address_erc20, to_address_depc, amount, depc_txid, depc_timestamp)";
 const SQL_CREATE_UNIQUE_INDEX_DEPC_WITHDRAW_ERC20_TXID: &str = "create unique index if not exists index__depc_withdraw_erc20_txid on depc_withdraw (erc20_txid)";
 const SQL_INSERT_DEPC_WITHDRAW: &str = "insert into depc_withdraw (erc20_txid, erc20_timestamp, from_address_erc20, to_address_depc, amount) values (?, ?, ?, ?, ?)";
 const SQL_UPDATE_DEPC_WITHDRAW: &str =
     "update depc_withdraw set depc_txid = ?, depc_timestamp = ? where erc20_txid = ?";
 const SQL_QUERY_BEST_HEIGHT: &str = "select height from blocks order by height desc limit 1";
-
 const SQL_QUERY_ADDRESSES_FROM_TX_INPUTS: &str =
     "select owner from coins where spent_txid = ? and is_spent = true";
-
 const SQL_QUERY_TXIDS_THOSE_INPUTS_CONTAIN_ADDRESS: &str =
     "select spent_txid from coins where owner = ? and is_spent = true group by spent_txid";
-
 const SQL_QUERY_BALANCE_OF_ADDRESS: &str =
     "select sum(value) from coins left join transactions on transactions.txid = coins.txid left join blocks on blocks.hash = transactions.block_hash where owner = ? and height <= ? and (spent_height is null or spent_height > ?)";
 
 const SQL_QUERY_BLOCK_TIME_BY_HEIGHT: &str = "select time from blocks where height = ?";
 
+/// Table `exchange_addresses`
 const SQL_CREATE_TABLE_EXCHANGE_ADDRESSES: &str = "create table if not exists exchange_addresses (address text primary key not null, analyzed_txid text not null)";
 const SQL_CREATE_INDEX_EXCHANGE_ADDRESSES_ANALYZED_TXID: &str = "create index if not exists index__exchange_addresses_analyzed_txid on exchange_addresses (analyzed_txid)";
 const SQL_INSERT_EXCHANGE_ADDRESSE: &str =
